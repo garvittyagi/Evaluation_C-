27941230de0881fa9261402a7142cf71dfb08eb8
garvittyagi <50312326+garvittyagi@users.noreply.github.com>
2022-10-08 15:59:29 +0530

    Add files via upload

---
+++ Eval_Q7/Eval_Q7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+
+namespace Eval_Q7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            int total = 5;
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine("Please enter your name");
+                string name = Console.ReadLine();
+                Console.WriteLine("Please enter your age");
+                int age = int.Parse(Console.ReadLine());
+
+                queue.Enqueue(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Person p = (Person)queue.Dequeue();
+                Console.WriteLine(p.ToString());
+            }
+        }
+
+        public class Person
+        {
+            public string Name;
+            public int Age;
+
+
+            public override string ToString()
+            {
+                return Name + " : " + Age;
+            }
+        }
+    }
+}
