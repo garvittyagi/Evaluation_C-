d71b2f9619feadf6eaf05d82f8a2463064e8699c
garvittyagi <50312326+garvittyagi@users.noreply.github.com>
2022-10-08 15:57:58 +0530

    Add files via upload

---
+++ Eval_Q1/eval/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace eval
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            int i, max, min, n;
+            Console.Write("Enter the number of array elements:");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter {0} elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Element - {0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            max = arr[0];
+            min = arr[0];
+
+            for (i = 1; i < n; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+
+
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.Write("Maximum element is : {0}\n", max);
+            Console.Write("Minimum element is : {0}\n\n", min);
+            Console.ReadKey();
+        }
+
+    }
+}
