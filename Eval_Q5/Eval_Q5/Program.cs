22f046fbd742fd6266b62c8528c5d3663ad6a9b8
garvittyagi <50312326+garvittyagi@users.noreply.github.com>
2022-10-08 15:52:18 +0530

    Add files via upload

---
+++ Eval_Q5/Eval_Q5/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Eval_Q5
+{
+    public delegate void function(double a);
+    internal class Program
+    {
+        
+        public void Table(double num)
+        {
+            double ans = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                ans = num * i;
+                Console.WriteLine(ans);
+            }
+
+        }
+
+        public void factorial(double num2)
+        {
+            double i, fact = 1;
+            for (i = 1; i <= num2; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("Factorial of " + num2 + " is: " + fact);
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            function obj = new function(program.factorial);
+            Console.WriteLine("Please enter the factor number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            obj.Invoke(num);
+
+
+            Console.WriteLine("Please enter the number for table");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            obj = program.Table;
+            obj.Invoke(num1);
+
+           
+
+            
+        }
+    }
+}
